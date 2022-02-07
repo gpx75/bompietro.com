97a4ad0a7e08f2addb7308d714991f37cd1578bd
Giancarlo Papa <giancarlo@gpx.dev>
2022-02-07 18:01:56 +0100

    + tailwind & daisyui

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <h1>Bompietro.com</h1>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="text-center hero-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Bompietro.com</h1>
+        <p class="mb-5">U Situ di Bompitrara</p>
+        <a href="https://github.com/gpx75/bompietro.com">
+          <button class="btn btn-primary">Ci stamu travagghiannu!</button>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./components/**/*.{vue,js}", "./composables/**/*.{js,ts}", "./layouts/**/*.vue", "./pages/**/*.vue", "./plugins/**/*.{js,ts}", "./utils/**/*.{js,ts}", "./app.vue"],
+  plugins: [require("daisyui")],
+};
