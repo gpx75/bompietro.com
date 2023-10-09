8f18e9937b0f0f62c0a5e9c70c53118f27a94859
Giancarlo Papa <giancarlo.papa@gmail.com>
2023-10-09 15:37:32 +0200

    removed button

--- app.vue
+++ app.vue
@@ -2,10 +2,10 @@
   <div class="hero min-h-screen bg-base-200">
     <div class="text-center hero-content">
       <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold">Qui Bompietro.com</h1>
+        <h1 class="mb-5 text-5xl font-bold">Bompietro.com</h1>
         <p class="mb-5">U Situ di Bompitrara</p>
         <a href="https://github.com/gpx75/bompietro.com">
-          <button class="btn btn-primary">Ci stamu travagghiannu!</button>
+          <!-- <button class="btn btn-primary">Ci stamu travagghiannu!</button> -->
         </a>
       </div>
     </div>
